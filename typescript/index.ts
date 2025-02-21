61fa90feb073053e543a3a5babd7d7a5833d943d
RY_2718 <ry.3607x5531@gmail.com>
2025-02-21 19:54:22 +0900

    add: TypeScript で遊んだ

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,2 +1,36 @@
-const msg: string = "Hello, World!";
-console.log(msg);
+type Product = {
+  name: string;
+  category: string;
+  price: number;
+};
+
+function groupProductsByCategory(
+  products: Product[]
+): Record<string, Product[]> {
+  return products.reduce(
+    (groups, product) => {
+      let group = groups[product.category];
+      if (!group) {
+        group = [];
+        groups[product.category] = group;
+      }
+      group.push(product);
+      return groups;
+    },
+    {} as Record<string, Product[]>
+  );
+}
+
+function main(): void {
+  const products: Product[] = [
+    { name: "Apple", category: "fruit", price: 100 },
+    { name: "Banana", category: "fruit", price: 150 },
+    { name: "Carrot", category: "vegetable", price: 200 },
+  ];
+
+  const grouped = groupProductsByCategory(products);
+
+  console.log(grouped);
+}
+
+main();
